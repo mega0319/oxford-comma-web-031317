adc88aa47832f53866240b85f2f98387570f90be
Naz Khan <nazmuskhan@yahoo.com>
2017-02-11 00:51:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    shortened = array.length - 3
+    partial_array = array[0..shortened]
+    final_piece = []
+    final_piece.push(array[-2..-1])
+    edited = final_piece.join(", and ")
+    edited2 = partial_array.join(", ")
+    edited2 + ", " + edited
+  end
 end
